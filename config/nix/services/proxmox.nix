4a52e5ac7fe8d3142d7deba65cd37dad89cd935b
Daniel36191 <dmoeller38@outlook.com>
2025-10-11 16:21:22 -0400

    proxmox

--- config/nix/services/proxmox.nix
+++ config/nix/services/proxmox.nix
@@ -1,15 +1,18 @@
 {
+  inputs,
+  pkgs,
   proxmox-nixos,
   ...
 }:{
   services.proxmox-ve = {
     enable = true;
+    package = inputs.proxmox-nixos.packages.${pkgs.system}.proxmox-nixos;
     ipAddress = "192.168.0.1";
   };
 
-  nixpkgs.overlays = [
-    proxmox-nixos.overlays.${system}
-  ];
+  # nixpkgs.overlays = [
+  #   proxmox-nixos.overlays.${pkgs.system}
+  # ];
 
   nix = {
     settings = {
