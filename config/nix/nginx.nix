f27d8256d23566dd1828fdc509221548475c8471
Daniel36191 <dmoeller38@outlook.com>
2025-10-12 16:35:03 -0400

    nginx

--- config/nix/nginx.nix
+++ config/nix/nginx.nix
@@ -1,27 +1,36 @@
-{ 
+{
   ...
 }:
 let
   redirectConfig = [
-    { domain = "home.lillypond.local"; port = "500"; } ## add homarr
+    { domain = "home.lillypond.local"; port = "500"; }
     { domain = "portainer.lillypond.local"; port = "9443"; }
     { domain = "proxmox.lillypond.local"; port = "8006"; }
   ];
 
   makeVhost = cfg: {
     serverName = cfg.domain;
+    enableACME = true;  # Automatically get SSL certificates
+    forceSSL = true;    # Force HTTPS
     locations."/" = {
-      return = "301 $scheme://lillypond.local:${cfg.port}$request_uri";
+      return = "301 https://lillypond.local:${cfg.port}$request_uri";
     };
   };
 
   vhosts = map makeVhost redirectConfig;
-in {
+in
+{
   services.nginx = {
     enable = true;
     virtualHosts = builtins.listToAttrs (map (vhost: {
       name = vhost.serverName;
       value = vhost;
     }) vhosts);
+  };
+  
+  # Enable ACME for Let's Encrypt certificates
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "your-email@example.com";
   };
 }
