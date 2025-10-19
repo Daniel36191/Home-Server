069da8b4d898786aca97d7f214d99392187f48ab
Daniel36191 <dmoeller38@outlook.com>
2025-10-19 16:33:14 -0400

    a

--- config/nix/core/containers.nix
+++ config/nix/core/containers.nix
@@ -5,12 +5,20 @@
 {
   virtualisation = {
     containers.enable = true;
-    oci-containers.backend = "podman";
-    podman = {
+    oci-containers.backend = "docker";
+    docker = {
       enable = true;
+      daemon.settings = {
+        features.cdi = true;
+      };
+      rootless = {
+        setSocketVariable = true;
+      };
     };
   };
 
   environment.systemPackages = with pkgs; [
+    docker-compose
+    docker-buildx
   ];
 }
