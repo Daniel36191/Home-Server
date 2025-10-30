53826b8f4efa2aa047ccb17365ee969658697324
Daniel36191 <dmoeller38@outlook.com>
2025-10-30 18:48:51 -0400

    nginx

--- config/nix/nginx.nix
+++ config/nix/nginx.nix
@@ -3,33 +3,34 @@
 }:
 let
   addr = "lillypond.local";
-  proxyConfig = [
-    { domain = "portainer.${addr}"; port = "9443"; secure = false; }
-    { domain = "home.${addr}"; port = "54321"; secure = false; default = true; }
-    { domain = "proxmox.${addr}"; port = "8006"; secure = true; }
-    { domain = "crafty.${addr}"; port = "8443"; secure = true; }
-  ];
-
-  makeVhost = cfg: {
-    default = cfg.default or false;
-    locations."/" = {
-      proxyPass = "${if cfg.secure == true then "https" else "http" }://${addr}:${cfg.port}/";
-      proxyWebsockets = true;
-    };
-    # forceSSL = cfg.secure;
-  };
-
-  vhosts = builtins.listToAttrs (map (cfg: {
-    name = cfg.domain;
-    value = makeVhost cfg;
-  }) proxyConfig);
-in {
+  # domain = "portainer"; port = "9443"; secure = false
+  # domain = "home"; port = "54321"; secure = false
+  # domain = "proxmox"; port = "8006"; secure = true
+  # domain = "crafty"; port = "8443"; secure = true
+in
+{
   services.nginx = {
     enable = true;
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    virtualHosts = vhosts;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    # other Nginx options
+    virtualHosts = {
+      "portiner.${addr}" =  {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:12345";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;" +
+            # required when the server wants to use HTTP Authentication
+            "proxy_pass_header Authorization;"
+            ;
+        };
+      };
+    };
   };
 }
