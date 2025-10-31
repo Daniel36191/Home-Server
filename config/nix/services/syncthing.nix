3c26fd0bcdfd34bc2fe5e3f6e8d353810a98d160
Daniel36191 <dmoeller38@outlook.com>
2025-10-31 15:25:27 -0400

    a

--- config/nix/services/syncthing.nix
+++ config/nix/services/syncthing.nix
@@ -12,12 +12,12 @@
     after = [ "network.target" ];
     wantedBy = [ "default.target" ];
     description = "File sync service";
+    script = ''
+      ${pkgs.syncthing}/bin/syncthing
+      syncthing cli config gui raw-address set 0.0.0.0:8384
+    '';
     serviceConfig = {
         Type = "simple";
-        script = ''
-        ${pkgs.syncthing}/bin/syncthing
-        syncthing cli config gui raw-address set 0.0.0.0:8384
-        '';
         Restart = "on-failure";
     };
   };
