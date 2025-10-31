e5b3e7a41d1890f5f37f58fb606d32807a590e21
Daniel36191 <dmoeller38@outlook.com>
2025-10-30 20:33:26 -0400

    a

--- config/nix/nginx.nix
+++ config/nix/nginx.nix
@@ -6,10 +6,10 @@
 let
   addr = "lillypond.local";
   proxyConfig = [
-    { domain = "portainer.${addr}"; port = "9443"; secure = false; }
-    { domain = "home.${addr}"; port = "54321"; secure = false; default = true; }
-    { domain = "proxmox.${addr}"; port = "8006"; secure = true; }
-    { domain = "crafty.${addr}"; port = "8443"; secure = true; sockets = true; }
+    { domain = "portainer"; port = "9443"; secure = false; }
+    { domain = "home"; port = "54321"; secure = false; default = true; }
+    { domain = "proxmox"; port = "8006"; secure = true; }
+    { domain = "crafty"; port = "8443"; secure = true; sockets = true; }
   ];
 
   mkCert = domain: pkgs.runCommand "cert-${domain}" { 
@@ -20,26 +20,26 @@ let
       mkdir -p $out
       cp cert.pem key.pem $out/
     '';
+
   makeVhost = cfg: {
     default = cfg.default or false;
     locations."/" = {
-      proxyPass = "${if cfg.secure == true then "https" else "http" }://${addr}:${cfg.port}/";
+      proxyPass = "${if cfg.secure then "https" else "http"}://${addr}:${toString cfg.port}/";
       proxyWebsockets = cfg.sockets or false;
-    } ++ (lib.optionals (cfg.secure == true){
-      extraConfig =
-        # required when the target is also TLS server with multiple hosts
-        "proxy_ssl_server_name on;" +
-        # required when the server wants to use HTTP Authentication
-        "proxy_pass_header Authorization;"
-      ;});
-    forceSSL = cfg.secure;
-  } ++ (lib.optionals (cfg.secure == true){
+    } // (lib.optionalAttrs cfg.secure {
+      extraConfig = ''
+        proxy_ssl_server_name on;
+        proxy_pass_header Authorization;
+      '';
+    });
+    forceSSL = cfg.secure or false;
+  } // (lib.optionalAttrs cfg.secure {
     sslCertificate = "${mkCert cfg.domain}/cert.pem";
     sslCertificateKey = "${mkCert cfg.domain}/key.pem";
   });
 
   vhosts = builtins.listToAttrs (map (cfg: {
-    name = cfg.domain;
+    name = "${cfg.domain}.${addr}";
     value = makeVhost cfg;
   }) proxyConfig);
 in {
