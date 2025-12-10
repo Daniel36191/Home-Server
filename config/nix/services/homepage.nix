1d7b451cf593b1db08c4df0ccfdf28de2357c408
Daniel36191 <dmoeller38@outlook.com>
2025-12-10 14:44:24 -0500

    a

--- config/nix/services/homepage.nix
+++ config/nix/services/homepage.nix
@@ -6,54 +6,44 @@
 }:
 let
   addr = "lillypond.local";
+  port = "54321";
+  mkUrl = url: secure: "${if secure then "https" else "http"}://${url}.${addr}";
 in
 {
   services.homepage-dashboard = {
     enable = true;
     openFirewall = true;
     listenPort = 54321;
-    allowedHosts = "${localipaddress}:54321,home.${addr},${addr}";
+    allowedHosts = "${localipaddress}:${port},home.${addr},${addr}";
     ## For Keys
-    environmentFile = "
-    ";
-
+    environmentFile = "";
+    
     bookmarks = let
       services = [
         {
-          enable = config.services.portiner.enable;
+          name = "portainer";
+          enable = config.services.portainer.enable or false;
           abbr = "PT";
           icon = "portainer";
-          url = "portiner";
+          url = "portainer";
           secure = true;
         }
       ];
-
-      enabledBookmarks = lib.filterAttrs (_: services: services.enable) services;
-
-      makeBookmarks = services: mkUrl:
-        let 
-          mkUrl = url: secure: "${if secure then "https" else "http"}://${url}.${addr}";
-          s = services;
-        in 
-        {
-          {
-            "${s.name}" = [
-              {
-                abbr = s.abbr;
-                icon = s.icon;
-                href = mkUrl s.url s.secure;
-              }
-            ];
-          }
-        };
+      
+      enabledServices = lib.filter (s: s.enable) services;
+      
+      makeBookmark = s: {
+        abbr = s.abbr;
+        icon = s.icon;
+        href = mkUrl s.url s.secure;
+      };
+      
     in [
       {
-        WebUIs = [ ## Catagory
-          makeBookmarks
-        ];
+        WebUIs = map (s: {
+          "${s.name}" = [ (makeBookmark s) ];
+        }) enabledServices;
       }
-
     ];
-
   };
 }
