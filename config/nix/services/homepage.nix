b2b9ebfb985bae1271d2582ecac0e13ff2b0dd11
Daniel36191 <dmoeller3998@gmail.com>
2025-12-30 17:46:30 -0500

    a

--- config/nix/services/homepage.nix
+++ config/nix/services/homepage.nix
@@ -29,7 +29,7 @@ in
     enable = true;
     openFirewall = true;
     listenPort = port;
-    allowedHosts = "${localipaddress}:${lib.toint port},home.${address},${address}";
+    allowedHosts = "${localipaddress}:${lib.toString port},home.${address},${address}";
     environmentFile = "";
     
     bookmarks = [
