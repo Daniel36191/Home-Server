e5f2945ae5a55c3001125e5e8ca8a07f266587d9
Daniel36191 <dmoeller3998@gmail.com>
2025-12-30 17:41:51 -0500

    a

--- config/nix/services/immich.nix
+++ config/nix/services/immich.nix
@@ -7,7 +7,7 @@
     openFirewall = true;
     port = services.immich.port;
     user = "immich";
-    mediaLocation = /services/immich;
+    mediaLocation = "/services/immich";
 
     machine-learning.enable = true; ## Dectect faces & objects
   };
