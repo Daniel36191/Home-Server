32b802cc47d905eef552f5bd4ea9fbf36b6c9ab7
Daniel36191 <dmoeller3998@gmail.com>
2026-01-10 16:19:53 -0500

    a

--- config/nix/caddy.nix
+++ config/nix/caddy.nix
@@ -18,7 +18,7 @@ let
       value = {
         extraConfig = ''
         encode gzip zstd
-        reverse_proxy ${if cfg.secure then "https" else "http"}://${address}:${toString cfg.port}
+        reverse_proxy ${if cfg.secure then "https" else "http"}://127.0.0.1:${toString cfg.port}
         '';
       };
     })
