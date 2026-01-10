62a94223df443e880f37f9885b517771db11e272
Daniel36191 <dmoeller3998@gmail.com>
2026-01-10 16:05:46 -0500

    a

--- config/nix/caddy.nix
+++ config/nix/caddy.nix
@@ -8,7 +8,6 @@
 let
   dir = "/services/caddy";
 
-
   ## Filter enabled services
   enabledServices = lib.filterAttrs (_: cfg: !cfg.no-proxy or false) (lib.filterAttrs (_: cfg: cfg.enable or false) services);
 
@@ -29,15 +28,15 @@ in {
   services.caddy = {
     enable = true;
     user = "caddy";
-    dataDir = "${dir}/data";
-    logDir = "${dir}/logs";
+    # dataDir = "${dir}/data";
+    # logDir = "${dir}/logs";
 
     virtualHosts = vhosts;
   };
 
-  systemd.tmpfiles.rules = [
-    "d ${dir} 0775 ${config.services.caddy.user} services -"
-    "d ${config.services.caddy.dataDir} 0775 ${config.services.caddy.user} services -"
-    "d ${config.services.caddy.logDir} 0775 ${config.services.caddy.user} services -"
-  ];
+  # systemd.tmpfiles.rules = [
+  #   "d ${dir} 0775 ${config.services.caddy.user} services -"
+  #   "d ${config.services.caddy.dataDir} 0775 ${config.services.caddy.user} services -"
+  #   "d ${config.services.caddy.logDir} 0775 ${config.services.caddy.user} services -"
+  # ];
 }
