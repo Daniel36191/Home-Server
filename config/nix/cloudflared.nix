a7136818b0558eae7eb4148d2d44955882efc909
Daniel36191 <dmoeller3998@gmail.com>
2026-01-19 15:29:06 -0500

    cloudflared

--- config/nix/cloudflared.nix
+++ config/nix/cloudflared.nix
@@ -5,8 +5,8 @@
   ... 
 }:
 let
-  ## Do CatchAll
-  mode = false;
+  ## Do Catchal
+  mode = true;
 
 
   ## Filter
@@ -17,23 +17,29 @@ let
   ) config.modules;
 
   ## Create Rules
-  ingress = lib.mapAttrsToList (name: cfg: {
-    hostname = "${cfg.domain}.${vars.sld}.${vars.tld}";
-    service = "${if cfg.secure or false then "https" else "http"}://127.0.0.1:${toString cfg.port}";
+  ingress = lib.mapAttrs' (name: cfg: {
+    name = "${cfg.domain}.${vars.sld}.${vars.tld}";
+    value = "${if cfg.secure or false then "https" else "http"}://127.0.0.1:${toString cfg.port}";
   }) enabledServices;
 in
 {
-  services.cloudflared = {
-    enable = true;
-    certificateFile = config.age.secrets."cloudflared-token".path;
-    tunnels."c8729276-c5da-4ca9-a170-a1535782266a" = {
-      credentialsFile = config.age.secrets."cloudflared-creds".path;
-      default = "http_status:404";
-      originRequest = {
-        noTLSVerify = true;
-      };
-      ingress = if !mode then ingress else {
-        "*.${vars.sld}.${vars.tld}" = "http://localhost:80";
+  options.modules.cloudflared = {
+    services = lib.mkOption { default = ingress; };
+};
+
+  config = {
+    services.cloudflared = {
+      enable = true;
+      certificateFile = config.age.secrets."cloudflared-token".path;
+      tunnels."c8729276-c5da-4ca9-a170-a1535782266a" = {
+        credentialsFile = config.age.secrets."cloudflared-creds".path;
+        default = "http_status:404";
+        originRequest = {
+          noTLSVerify = true;
+        };
+        ingress = if !mode then ingress else {
+          "*.${vars.sld}.${vars.tld}" = "http://localhost:80";
+        };
       };
     };
   };
