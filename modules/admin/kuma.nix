6e21ccc099c17a712bca1c681736401df1cdd82a
Daniel36191 <dmoeller3998@gmail.com>
2026-02-01 15:33:35 -0500

    set dir with link

--- modules/admin/kuma.nix
+++ modules/admin/kuma.nix
@@ -13,10 +13,18 @@ in
       enable = true;
       appriseSupport = true;
       settings = {
-        DATA_DIR = mod.data-directory;
         PORT = mod.port;
         HOST="127.0.0.1";
       };
     };
+
+    ## Has to link becasue of systemd hardneing see https://github.com/NixOS/nixpkgs/issues/324105
+    system.activationScripts.uptime-kuma = ''
+    if [[ -d ${mod.data-directory} ]]; then
+      mkdir -p /var/lib/private
+      chmod 700 /var/lib/private
+      ln -s ${mod.data-directory} /var/lib/private/uptime-kuma
+    fi
+    '';
   };
 }
