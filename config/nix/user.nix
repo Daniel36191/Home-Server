9bca320a3fcdea7300063f6466b0e5079e620b83
Daniel36191 <dmoeller3998@gmail.com>
2026-02-06 22:37:25 -0500

    ssh fix

--- config/nix/user.nix
+++ config/nix/user.nix
@@ -111,7 +111,7 @@
     settings = {
       PasswordAuthentication = true;
       AllowUsers = null; ## Allows all users by default. Can be [ "user1" "user2" ]
-      UseDns = true;
+      UseDns = false;
       X11Forwarding = false;
       PermitRootLogin = "yes"; ## "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
       PubkeyAuthentication = "yes";
