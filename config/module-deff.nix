eb9c9c13ad8f12a450bc951f0b7305f82a40d4aa
Daniel36191 <dmoeller3998@gmail.com>
2026-07-05 01:31:58 -0400

    Auto module-deff list

--- config/module-deff.nix
+++ config/module-deff.nix
@@ -4,21 +4,26 @@
 }:
 with lib;
 let
-  module-list = [
-    "authentik"
-    "portainer"
-    "proxmox"
-    "syncthing"
-    "copyparty"
-    "home-assistant"
-    "homepage"
-    "immich"
-    "minecraft"
-    "radicale"
-    "vaultwarden"
-    "kuma"
-    "forgejo"
-  ];
+  modulesFolder = ../modules;
+  nixFiles = builtins.filter (p: lib.hasSuffix ".nix" (toString p)) (
+    lib.filesystem.listFilesRecursive modulesFolder
+  );
+  parseFile =
+    path:
+    let
+      str = toString path;
+      relPath = builtins.elemAt (builtins.split "/modules/" str) 2;
+      segs = lib.splitString "/" relPath;
+      rest = builtins.tail segs; # drop section
+      stem = lib.removeSuffix ".nix" (lib.last rest);
+      group = if builtins.length rest > 1 then builtins.head rest else null;
+    in
+    {
+      inherit stem group;
+      depth = builtins.length rest;
+    };
+  parsed = map parseFile nixFiles;
+  servicesList = lib.unique (map (p: p.stem) (builtins.filter (p: p.depth == 1) parsed));
 
   options = listToAttrs (
     map (name: {
@@ -47,7 +52,7 @@ let
         };
 
       };
-    }) module-list
+    }) servicesList
   );
 in
 {
