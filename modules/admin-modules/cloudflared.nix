a41870a5a1d6092b889d964e6d284aaa3ce548a7
Daniel36191 <dmoeller3998@outlook.com>
2026-07-13 12:58:10 -0400

    revert cloudflared custom module

---
+++ modules/admin-modules/cloudflared.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  vars,
+  ...
+}:
+
+## WARN: Doesn't create routes via dash.cloudflare.com => Networking => Tunnels => Routes
+
+let
+  id = "43387887-077c-4587-8be7-58fcc0f35558";
+in
+{
+  config = {
+    services.cloudflared = {
+      enable = true;
+      certificateFile = config.age.secrets."cloudflared-token".path;
+      tunnels."${id}" = {
+        credentialsFile = config.age.secrets."cloudflared-creds".path;
+        default = "http_status:404";
+        originRequest = {
+          noTLSVerify = true;
+        };
+        ingress = {
+          "*.${vars.sld}.${vars.tld}" = "http://localhost";
+        };
+      };
+    };
+  };
+}
