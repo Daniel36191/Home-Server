c62f0bd63f9bbc7ba86ef90bab56897d9ff2a294
Daniel36191 <dmoeller3998@outlook.com>
2026-07-19 13:54:48 -0400

    update authentik

--- modules/services/authentik.nix
+++ modules/services/authentik.nix
@@ -8,14 +8,14 @@ with lib;
 let
   mod = config.modules.authentik;
 in
-{  
+{
   config = mkIf mod.enable {
     services.authentik = {
       enable = true;
       environmentFile = config.age.secrets."authentik-env".path;
       nginx = {
         enable = if config.services.caddy.enable then false else true;
-        host = "${mod.proxy.domain}.${vars.sld}.${if mod.proxy.public then vars.tld else "local" }";
+        host = "${mod.proxy.domain}.${vars.sld}.${if mod.proxy.public then vars.tld else "local"}";
       };
       settings = {
         disable_startup_analytics = true;
