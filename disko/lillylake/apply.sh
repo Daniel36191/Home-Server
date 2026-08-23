47a963987068338b74d8643322c4b02bef1068ed
Daniel36191 <dmoeller3998@gmail.com>
2026-08-23 18:23:21 -0400

    lillylake:

--- disko/lillylake/apply.sh
+++ disko/lillylake/apply.sh
@@ -2,4 +2,6 @@
 echo "Warning! This will wipe all raid drives. Any key to proceeded: "
 read
 echo "Writing..."
-sudo nix run github:nix-community/disko -- --mode destroy,format,mount ./disko-config.nix
+sudo nix run github:nix-community/disko -- --mode destroy,format,mount ./disko.nix
+sudo zpool status
+sudo zfs list
